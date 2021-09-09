3c07080469ef11bf77a828c13dd2d3f413c01e9b
Naman Arora <naman.arora@citymall.live>
2021-09-09 18:23:55 +0530

    worked upon animation

--- App.js
+++ App.js
@@ -6,8 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
+import React,{useEffect} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -16,6 +15,8 @@ import {
   Text,
   useColorScheme,
   View,
+  TouchableOpacity,
+  Image
 } from 'react-native';
 
 import {
@@ -25,88 +26,121 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import Animated, { useSharedValue,  useAnimatedStyle, withSpring} from 'react-native-reanimated';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+const HANDLE_WIDTH = 20;
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+const App=()=>{
+  const sliderWidth=useSharedValue(0);
+  const progress=useSharedValue(0);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const animatedHandleStyle=useAnimatedStyle(()=>{
+    return {
+      transform: [{translateX: progress.value - HANDLE_WIDTH/2}],
+    }
+  })
+  
+  useEffect(()=>{
+    const interval = setInterval(()=>{
+      progress.value=withSpring(Math.random()*sliderWidth.value);
+    },1000)
+    return ()=>{
+      clearInterval(interval);
+    }
+
+  },[])
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+    <View
+    style={{
+      flex: 1,
+      backgroundColor: "blue",
+      justifyContent: "flex-end",
+      borderRadius: 10,
+    }}
+    onLayout={(e) => {
+      sliderWidth.value = e.nativeEvent.layout.width;
+    }}
+  >
+    <Animated.View
+      style={[
+        { height: 100,
+          width: HANDLE_WIDTH,
+          backgroundColor: 'red',
+          borderRadius: 10,
+          position: "absolute",
+          bottom: 20,
+          top: 520,
+        },
+        animatedHandleStyle,
+      ]}
+    />
+  </View>
+  )
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+
+}
+
+
+
+// const App= () => {
+//   const randomNumber = useSharedValue(1000);
+
+//   const style = useAnimatedStyle(() => {
+//     return { width: randomNumber.value, height: randomNumber.value };
+//   });
+//   const isDarkMode = useColorScheme() === 'dark';
+
+//   const backgroundStyle = {
+//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+//   };
+
+//   return (
+    
+//       <View
+//       style={{
+//         flex: 1,
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//         flexDirection: 'column',
+//         backgroundColor: '#7CA1B4',
+//       }}
+//     >
+//       <TouchableOpacity
+//         onPress={() => {
+//           console.log('hey i am in ', withSpring(Math.random() * 350))
+//           randomNumber.value = withSpring(Math.random() * 350);  
+//           console.log('hey ', randomNumber.value)
+//         }}
+//       >
+//         <Animated.Image
+//           source={require('./pic.png')}
+//           resizeMode="contain"
+//           style={style}
+//         />
+//       </TouchableOpacity>
+//     </View>
+  
+//   );  
+// };
+
+// const styles = StyleSheet.create({
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+// });
 
 export default App;
