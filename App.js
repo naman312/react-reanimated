a70fad326015f82806e63c1de158b9ba94ef77a0
Naman Arora <naman.arora@citymall.live>
2021-09-10 11:43:55 +0530

    working over react acimation Animated API

--- App.js
+++ App.js
@@ -6,80 +6,80 @@
  * @flow strict-local
  */
 
-import React,{useEffect} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  TouchableOpacity,
-  Image
-} from 'react-native';
+// import React,{useEffect} from 'react';
+// import {
+//   SafeAreaView,
+//   ScrollView,
+//   StatusBar,
+//   StyleSheet,
+//   Text,
+//   useColorScheme,
+//   View,
+//   TouchableOpacity,
+//   Image
+// } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import Animated, { useSharedValue,  useAnimatedStyle, withSpring} from 'react-native-reanimated';
+// import {
+//   Colors,
+//   DebugInstructions,
+//   Header,
+//   LearnMoreLinks,
+//   ReloadInstructions,
+// } from 'react-native/Libraries/NewAppScreen';
+// import Animated, { useSharedValue,  useAnimatedStyle, withSpring} from 'react-native-reanimated';
 
-const HANDLE_WIDTH = 20;
+// const HANDLE_WIDTH = 20;
 
-const App=()=>{
-  const sliderWidth=useSharedValue(0);
-  const progress=useSharedValue(0);
+// const App=()=>{
+//   const sliderWidth=useSharedValue(0);
+//   const progress=useSharedValue(0);
 
-  const animatedHandleStyle=useAnimatedStyle(()=>{
-    return {
-      transform: [{translateX: progress.value - HANDLE_WIDTH/2}],
-    }
-  })
+//   const animatedHandleStyle=useAnimatedStyle(()=>{
+//     return {
+//       transform: [{translateX: progress.value - HANDLE_WIDTH/2}],
+//     }
+//   })
   
-  useEffect(()=>{
-    const interval = setInterval(()=>{
-      progress.value=withSpring(Math.random()*sliderWidth.value);
-    },1000)
-    return ()=>{
-      clearInterval(interval);
-    }
+//   useEffect(()=>{
+//     const interval = setInterval(()=>{
+//       progress.value=withSpring(Math.random()*sliderWidth.value);
+//     },1000)
+//     return ()=>{
+//       clearInterval(interval);
+//     }
 
-  },[])
+//   },[])
 
-  return (
-    <View
-    style={{
-      flex: 1,
-      backgroundColor: "blue",
-      justifyContent: "flex-end",
-      borderRadius: 10,
-    }}
-    onLayout={(e) => {
-      sliderWidth.value = e.nativeEvent.layout.width;
-    }}
-  >
-    <Animated.View
-      style={[
-        { height: 100,
-          width: HANDLE_WIDTH,
-          backgroundColor: 'red',
-          borderRadius: 10,
-          position: "absolute",
-          bottom: 20,
-          top: 520,
-        },
-        animatedHandleStyle,
-      ]}
-    />
-  </View>
-  )
+//   return (
+//     <View
+//     style={{
+//       flex: 1,
+//       backgroundColor: "blue",
+//       justifyContent: "flex-end",
+//       borderRadius: 10,
+//     }}
+//     onLayout={(e) => {
+//       sliderWidth.value = e.nativeEvent.layout.width;
+//     }}
+//   >
+//     <Animated.View
+//       style={[
+//         { height: 100,
+//           width: HANDLE_WIDTH,
+//           backgroundColor: 'red',
+//           borderRadius: 10,
+//           position: "absolute",
+//           bottom: 20,
+//           top: 520,
+//         },
+//         animatedHandleStyle,
+//       ]}
+//     />
+//   </View>
+//   )
 
 
-}
+// }
 
 
 
@@ -143,4 +143,77 @@ const App=()=>{
 //   },
 // });
 
+
+
+import React, { useRef } from "react";
+import { Animated, Text, View, StyleSheet, Button, SafeAreaView } from "react-native";
+
+const App = () => {
+  // fadeAnim will be used as the value for opacity. Initial Value: 0
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  const fadeIn = () => {
+    // Will change fadeAnim value to 1 in 5 seconds
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 5000,
+      useNativeDriver: true
+    }).start();
+  };
+
+  const fadeOut = () => {
+    // Will change fadeAnim value to 0 in 3 seconds
+    Animated.timing(fadeAnim, {
+      toValue: 0,
+      duration: 3000,
+      useNativeDriver: true
+
+    }).start();
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Animated.View
+        style={[
+          styles.fadingContainer,
+          {
+            // Bind opacity to animated value
+            opacity: fadeAnim
+          }
+        ]}
+      >
+        <Text style={styles.fadingText}>Fading View!</Text>
+      </Animated.View>
+      <View style={styles.buttonRow}>
+        <Button title="Fade In View" onPress={fadeIn} />
+        <Button title="Fade Out View" onPress={fadeOut} />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  fadingContainer: {
+    padding: 20,
+    backgroundColor: "powderblue"
+  },
+  fadingText: {
+    fontSize: 28
+  },
+  buttonRow: {
+    flexBasis: 100,
+    justifyContent: "space-evenly",
+    marginVertical: 16
+  }
+});
+
 export default App;
+
+
+
+
